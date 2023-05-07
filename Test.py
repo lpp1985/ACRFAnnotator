7325ab4d62bea13e38dcad03919e558c8c6f8dee
Your Name <you@example.com>
2023-05-07 12:22:26 +0800

    Adding New Ddict Class

--- Test.py
+++ Test.py
@@ -12,20 +12,24 @@ class Ddict(defaultdict):
             self.__getitem__(key)._data.add(value)
             # print(self._data)# add string to set
         elif isinstance(value, list):
-            self._data = set()
+            self.__getitem__(key)._data=set()
             for v in value:
 
                 if len(v)>0 :
-                    self._data.add(v)  # add string to set
+                    self.__getitem__(key)._data.add(v)  # add string to set
         else:
             super().__setitem__(key, value) # set value to Ddict object
     def data(self):
         return self._data # return a set
 x = Ddict()
-x["a"]["B"] = "hello"
-# x["a"]["B"]["D"] = ["world", "python"]
-print(x["a"]["B"].data()git
-print(x["a"]["E"].data())
-print(x["a"]["B"]["x"])
 
-print(x["a"]["B"])
+# x["a"]["B"]
+x["a"]["B"] = "hello"
+x["a"]["B"]["D"] = ["world","", "python"]
+#x["a"]["B"]
+# x[""][""][""]["F4"]
+# x[""][""][""]["F4"]="ceeeeasd"
+# x[""][""][""]["F4"]
+# x["a"]["B"]["D"] = ["world", "python"]
+print(x["a"]["B"].data())
+print(x["a"]["B"]["D"].data() )
